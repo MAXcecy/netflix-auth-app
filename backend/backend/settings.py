19fb7d7b9a1e2f85c1b5ce97af465a8fefc8dd31
sergiocruz <sergiocruz2511@gmail.com>
2025-07-17 09:55:36 -0600

    termine el backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
